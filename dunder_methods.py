6571e57ad6e815ff320efd0409e182c0daac90e2
RajdeepBiswas89 <rajdeepbiswas403@gmail.com>
2024-07-28 20:52:18 +0530

    dunder methods

---
+++ dunder_methods.py
@@ -0,0 +1,12 @@
+class magicmethods:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def __repr__(self):
+        return f"the name of the employee is {self.name} and his salary is {self.salary} (__repr__ method)"
+    def __str__(self):
+        return f"the name of the employee is {self.name} and his salary is {self.salary}"
+obj1=magicmethods("rajdeep",70000)
+obj2=magicmethods("harry",20000)
+print(obj1)
+print(obj2)
